f80c07af8ea44c36ba04ad8a1d91c6095499c4b7
Kyle553 <elkorda204@gmail.com>
2025-04-30 22:02:55 +0300

    Offline typing tool

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const text = "";
+const text = "All visuals and music in this video are 100% crafted by talented human artists, without the use of AI. We’re committed to delivering genuine, hand-made creations for our audience to enjoy.";
 const currentIndex = 0;
-let typedChar = "";
+// let typedChar = "";
+
+(function renderText ( text ) {
+  let words = [];
+  words = text.trim().split(/\s+/);
+  console.log(words);
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].split("");
+  }
+  console.log(words);
+
+
+  words.forEach((element, index) => {
+    const createdDiv = document.createElement("div");
+    console.log(element);
+    
+    for (let i = 0; i < element.length; i++) {
+      const createdSpan = document.createElement("span");
+      createdSpan.textContent = element[i];
+      createdDiv.appendChild(createdSpan);
+    }
+
+
+    const target_char = document.getElementById("target_char");
+    target_char.appendChild(createdDiv);
+
+  });
+})(text);
 
 
 
@@ -8,8 +36,7 @@ let typedChar = "";
 
 
 
+// document.addEventListener("keydown", (event) => {
 
-document.addEventListener("keydown", (event) => {
-
-});
+// });
 
