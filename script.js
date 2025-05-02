7732829ed84e76ae8a4ea4e50c0fc4f0d6279543
Kyle553 <elkorda204@gmail.com>
2025-05-02 05:53:07 +0300

    Offline typing tool

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const text = "All visuals and music in this video are 100% crafted by talented human artists, without the use of AI. We’re committed to delivering genuine, hand-made creations for our audience to enjoy.";
-const currentIndex = 0;
-// let typedChar = "";
+const text = "All visuals and music in this video ";
+// are 100% crafted by talented human artists, without the use of AI. We’re committed to delivering genuine, hand-made creations for our audience to enjoy.
+let charIndex = 0;
+let wordIndex = 0;
+let words = [];
 
 const target_char = document.getElementById("target_char");
 
 (function renderText ( text ) {
-  let words = [];
   words = text.trim().split(/\s+/);
   console.log(words);
 
@@ -29,9 +30,49 @@ const target_char = document.getElementById("target_char");
   });
 })(text);
 
+target_char.addEventListener("click", (event) => {
+  const allWords = document.querySelectorAll(".word");
+  console.log(words.length);
+  console.log(allWords[2]);
 
+  document.addEventListener("keyup", (event) => {
+    const allWords = document.querySelectorAll(".word");
+    const currentWordDiv = allWords[wordIndex];
+    const charSpans = currentWordDiv.querySelectorAll("span");
+    const currentSpan = charSpans[charIndex];
 
-// target_char.addEventListener("click", (event) => {
-//   target_char.style.backgroundColor = "white"
-// });
+    if (event.key !== "Backspace") {
+      if (event.key.length === 1) {
+        if (charIndex < words[wordIndex].length) {
+          if (words[wordIndex][charIndex] === event.key ) {
+            currentSpan.classList.add("correct");
+            charIndex += 1;
+            console.log("TAAAAAAAAAAAAK");
+            console.log(words[wordIndex][charIndex]);
+          } else {
+            console.log("NIIIIIIIIIIIIIII");
+            console.log(words[wordIndex][charIndex]);
+            currentSpan.classList.add("incorrect");
+            charIndex += 1;
+          }
+        } else {
+          if (event.key === " ") {
+            wordIndex += 1;
+            charIndex = 0;
+          } else {
+  
+          }
+        }
+      }
+    } else {
+      if (charIndex < (words[wordIndex].length + 1)) {
+        charIndex -= 1;
+        currentSpan.classList.remove("correct", "incorrect");
+    
+        console.log(words[wordIndex][charIndex + 1]);
+      }
+      
+    }
+  });
+});
 
