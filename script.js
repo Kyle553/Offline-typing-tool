56c4be79b11dc2d1e0b9505ddf266436649c9307
Kyle553 <elkorda204@gmail.com>
2025-05-06 23:31:27 +0300

    Offline typing tool

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const text = "All visuals and music in this video ";
+const text = "123 All visuals and music in this video ";
 // are 100% crafted by talented human artists, without the use of AI. We’re committed to delivering genuine, hand-made creations for our audience to enjoy.
 let charIndex = 0;
 let wordIndex = 0;
 let words = text.trim().split(/\s+/);
-console.log(words);
 
 const target_char = document.getElementById("target_char");
 
@@ -21,45 +20,56 @@ const target_char = document.getElementById("target_char");
   }
 })();
 
+
 // target_char.focus();
 // замінити click на focus
 target_char.addEventListener("click", () => { 
   //замінити document на target_char
-  document.addEventListener("keydown", (event) => {
-    const allWords = document.querySelectorAll(".word");
-    const currentWord = allWords[wordIndex];
-    const allChar = currentWord.querySelectorAll("span");
-    let currentSpan = allChar[charIndex];
-
-    if (event.key === " " && wordIndex < (words.length - 1)) {
-      wordIndex += 1;
-      charIndex = 0;
-      return;
-    } 
-
-    // if (event.key === "Backspace") {
-    //   if (charIndex === 0 && wordIndex !== 0) {
-    //     wordIndex -= 1
-    //     charIndex = words[wordIndex].length;
-    //   }
-    //   if (charIndex > 0) {
-    //     charIndex -= 1;
-    //     currentSpan = allChar[charIndex];
-    //     currentSpan.classList.remove("correct", "incorrect");
-    //   }
-    //   return;
-    // }
-
-    if (event.key.length !== 1) {
-      return;
-    }
-
-    if (charIndex < words[wordIndex].length) {
-        const className = words[wordIndex][charIndex] === event.key ? "correct" : "incorrect"
-        currentSpan.classList.add(className);
-        charIndex += 1;
-    }
-  });
+  document.addEventListener("keydown", typingText);
 });
 
-// currentWordDOM, currentSpanDOM, currentWord
+function typingText (event) {
+  const allWords = document.querySelectorAll(".word");
+  const currentWord = allWords[wordIndex];
+  const allChar = currentWord.querySelectorAll("span");
+  let currentSpan = allChar[charIndex];
+
+  let lastChar = null;
+  let lastWord = null;
+
+  if (event.key === " " && wordIndex < (words.length - 1)) {
+    wordIndex += 1;
+    charIndex = 0;
+    lastChar = charIndex;
+    lastWord = wordIndex;
+    return;
+  } 
+
+  // if (lastWord === words[wordIndex - 1] && lastChar !== words[wordIndex - 1].at(-1))
+  if (event.key === "Backspace") {
+    if (charIndex === 0 && wordIndex > 0) {
+      wordIndex -= 1;
+      charIndex = words[wordIndex].length;
+      return;
+    } 
+    
+    if (charIndex > 0) {  
+      charIndex -= 1;
+      currentSpan = allChar[charIndex];
+      currentSpan.classList.remove("correct", "incorrect");
+      return;
+    }
+  }
+
+  if (event.key.length !== 1) {
+    return;
+  }
+
+  if (charIndex < words[wordIndex].length) {
+      const className = words[wordIndex][charIndex] === event.key ? "correct" : "incorrect"
+      currentSpan.classList.add(className);
+      charIndex += 1;
+      return;
+  }
+  // currentWordDOM, currentSpanDOM, currentWord
+}
