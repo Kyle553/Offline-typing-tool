5fd2766f5365de871262faaa9e4a66b926212755
Kyle553 <elkorda204@gmail.com>
2025-05-14 09:05:47 +0300

    Offline typing tool -40%

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const text = "123 All visuals and music in this video ";
+const text = "123 All visuals and music in this video";
 // are 100% crafted by talented human artists, without the use of AI. We’re committed to delivering genuine, hand-made creations for our audience to enjoy.
-const words = text.trim().split(/\s+/);
+const words = text.match(/\S+|\s+/g)
+words.forEach((element, index, arr) => {
+  arr[index] = element.replace(/ /g, "\u00A0");
+});
+console.log(words)
 let wordIndex = 0;
 let charIndex = 0;
-let unfinishedWords = [];
-let extraChars = [];
+let totalChars = 0;
 
-const target_char = document.getElementById("target_char");
+const target_charDOM = document.getElementById("target_char");
 
-(function renderText () {
+(() => {
   for (let element of words) {
     const createdDiv = document.createElement("div");
     createdDiv.classList.add("word");
-    target_char.appendChild(createdDiv);
+    target_charDOM.appendChild(createdDiv);
 
     for (let char of element) {
       const createdSpan = document.createElement("span");
@@ -23,116 +26,74 @@ const target_char = document.getElementById("target_char");
 })();
 
 
-// target_char.focus();
+
+for (let element of words) {
+  totalChars += element.length;
+}
+
+// target_charDOM.focus();
 // замінити click на focus
-target_char.addEventListener("click", () => { 
-  //замінити document на target_char
+target_charDOM.addEventListener("click", () => {
+  //замінити document на target_charDOM
   document.addEventListener("keydown", typingText);
 });
 
 
-function typingText (event) {
-  function getCharIndex(array, wordIndex) {
-    return array.find((element) => element.indexWord === wordIndex)?.indexChar;
-  }
-
-  function getElemIndex(array, wordIndex) {
-    return array.findIndex((element) => element.indexWord === wordIndex);
-  }
-
+function typingText(event) {
   const allWordsDOM = document.querySelectorAll(".word");
   let currentWordDOM = allWordsDOM[wordIndex];
   const allCharsDOM = currentWordDOM.querySelectorAll("span");
   let currentCharDOM = allCharsDOM[charIndex];
 
   let currentWord = words[wordIndex];
-  let unfElementIndex = null;
+  // console.log(charIndex)
 
-  if (event.key === " ") {
-    spase();
-    return;
-  }
-  
   if (event.key === "Backspace") {
     backspace();
     return;
   }
-  
+
   if (event.key.length === 1) {
-    classes();
+    clas();
     return;
   }
 
-  function spase () {
-    if (wordIndex === (words.length - 1)) {
-      return;
-    } 
-
-    if (charIndex < currentWord.length) {
-      currentWordDOM.classList.add("unfinishedWords");
-
-      unfElementIndex = getElemIndex(unfinishedWords, wordIndex);
-      if (unfElementIndex >= 0) {
-        unfinishedWords[unfElementIndex].indexChar = charIndex;
-      } else {
-        unfinishedWords.push({indexChar: charIndex, indexWord: wordIndex});
-      }
-    }
-
-    wordIndex += 1;
-    currentWord = words[wordIndex];
-    charIndex = 0;
-  };
-  
-  function backspace () {
-    const unfWord = currentWordDOM.classList.contains("unfinishedWords");
-    
-    if (wordIndex === 0 && charIndex === 0 && unfWord) {
-      currentWordDOM.classList.remove("unfinishedWords");
-    }
-
+  function backspace() {
     if (charIndex === 0 && wordIndex > 0) {
-      if (unfWord) {
-        currentWordDOM.classList.remove("unfinishedWords");
-        unfElementIndex = getElemIndex(unfinishedWords, wordIndex);
-        unfinishedWords.splice(unfElementIndex, 1);
-      }
 
       wordIndex -= 1;
       currentWord = words[wordIndex];
-      
-      let unfCharIndex = null;
-      currentWordDOM = allWordsDOM[wordIndex];
-      if (unfWord) {
-        unfCharIndex = getCharIndex(unfinishedWords, wordIndex);
-        charIndex = unfCharIndex;
-      }
-    
-      unfCharIndex === null ? charIndex = currentWord.length : null;
-      return;
-    } 
-    
-    if (charIndex > 0) { 
-      charIndex -= 1;
-      currentCharDOM = allCharsDOM[charIndex];
-    
-      charIndex < currentWord.length ? currentCharDOM.classList.remove("correct", "incorrect") : currentCharDOM.remove();
-    } 
-  };
-  
-  function classes () {
-    if (charIndex < currentWord.length) {
-      const className = currentWord[charIndex] === event.key ? "correct" : "incorrect";
-      currentCharDOM.classList.add(className);
-      charIndex += 1;
-    } else {
-      const createdExtraSpan = document.createElement("span");
-      createdExtraSpan.textContent = event.key;
-      createdExtraSpan.classList.add("incorrect", "extra");
-      currentWordDOM.appendChild(createdExtraSpan);
 
-      extraChars.push({indexChar: charIndex, indexWord: wordIndex});
-      charIndex += 1;
+      charIndex = currentWord.length - 1;
+      return;
+    }
+
+    if (charIndex > 0) {
+      if (charIndex === currentWord.length - 1) {
+        currentCharDOM.classList.remove("correct", "incorrect");
+        currentCharDOM.classList.length === 0 ? charIndex -= 1 : null;
+        return;
+      }
+
+      currentCharDOM = allCharsDOM[charIndex];
+      currentCharDOM.classList.remove("correct", "incorrect");
+      charIndex > 0 ? charIndex -= 1 : null;
     }
   };
+  
+  // function clas() {
+  //   if (charIndex <= (currentWord.length - 1)) {
+  //     const className = currentWord[charIndex] === event.key ? "correct" : "incorrect";
+  //     currentCharDOM.classList.add(className);
+  //     if (charIndex < (currentWord.length - 1)) {
+  //       charIndex += 1;
+  //       return;
+  //     }
+
+  //     if (wordIndex === (words.length - 1)) {
+  //       wordIndex += 1;
+  //       charIndex = 0;
+  //     }
+  //   } 
+  // };
 }
