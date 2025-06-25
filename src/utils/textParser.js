b81b191ecc3b514096af0c7752af54eadee25bd2
Kyle553 <elkorda204@gmail.com>
2025-06-25 23:59:33 +0300

    Add random punctuation generation to loremGenerator function (not finished)

--- src/utils/textParser.js
+++ src/utils/textParser.js
@@ -18,15 +18,64 @@ function newDefaultText(text) {
   defaultText = text;
 }
 
+// =========================================================================================
 function loremGenerator(num) {
+  function getRandomPunctuationOrNone() {
+    // Шанси в відсотках
+    const punctuationsChance = {
+      comma: {
+        chance: 7,       
+        symbol: ','      
+      },
+      period: {
+        chance: 4,
+        symbol: '.'
+      },
+      doubleQuotes: {
+        chance: 2,
+        symbol: '"'
+      },
+      hyphen: {
+        chance: 1,
+        symbol: '-'
+      },
+      questionMark: {
+        chance: 0.6,
+        symbol: '?'
+      },
+      exclamationMark: {
+        chance: 0.5,
+        symbol: '!'
+      },
+      colon: {
+        chance: 0.4,
+        symbol: ':'
+      },
+      brackets: {
+        chance: 0.3,
+        symbol: '()'
+      }
+    };
+
+    for ([key, {chance, symbol}] of Object.entries(punctuationsChance)) {
+
+    }
+
+
+
+
+
+
+  }
+
   const defaultLorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
   const words = defaultLorem
     .toLowerCase()
     .replace(/[^\wа-яіїєґ']+/g, ' ') 
     .trim()
     .split(/\s+/);
-    
-  let templorem = null;
+
+  let previousWord = null;
   let lorem = [];
   let rndNum = null;
 
@@ -41,46 +90,45 @@ function loremGenerator(num) {
     }
 
     if (i === 0) {
-      templorem = lorem[i];
+      previousWord = lorem[i];
     }
     
     if (i > 1 && i < num - 1) {
-      templorem = lorem[i - 1]
+      previousWord = lorem[i - 1]
     }
-
 
 // зробити рандомні крапки і коми
 
-    // Зробити перший символ першого слова великим
-    if (lorem[0][0] === lorem[0][0].toLowerCase()) {
-      lorem[0] = lorem[0][0].toUpperCase() + lorem[0].slice(1);
-    }
+    // // Зробити перший символ першого слова великим
+    // if (lorem[0][0] === lorem[0][0].toLowerCase()) {
+    //   lorem[0] = lorem[0][0].toUpperCase() + lorem[0].slice(1);
+    // }
 
-    // Додати в кінці крапку
-    if (i === (num - 1) && lorem[i].at(-1) !== "." ) {
-      lorem[i] = lorem[i] + ".";
-    }
+    // // Додати в кінці крапку
+    // if (i === (num - 1) && lorem[i].at(-1) !== "." ) {
+    //   lorem[i] = lorem[i] + ".";
+    // }
     
-    // Замінити в кінці кому на крапку
-    if (i === (num - 1) && (lorem[i].at(-1) === "," && lorem[i].at(-1) !== "." )) {
-      lorem[i] = lorem[i].slice(0, -1) + ".";
-    }
+    // // Замінити в кінці кому на крапку
+    // if (i === (num - 1) && (lorem[i].at(-1) === "," && lorem[i].at(-1) !== "." )) {
+    //   lorem[i] = lorem[i].slice(0, -1) + ".";
+    // }
     
-    // Додай крапку позаду якщо слово починається з великою
-    if (lorem[i][0] === lorem[i][0].toUpperCase() && templorem.at(-1) !== "." && i > 0) {
-      if (templorem.at(-1) !== ",") {
-        lorem[i - 1] = lorem[i - 1] + ".";
-      }
+    // // Додай крапку позаду якщо слово починається з великою
+    // if (lorem[i][0] === lorem[i][0].toUpperCase() && previousWord.at(-1) !== "." && i > 0) {
+    //   if (previousWord.at(-1) !== ",") {
+    //     lorem[i - 1] = lorem[i - 1] + ".";
+    //   }
       
-      if (templorem.at(-1) === ",") {
-        lorem[i - 1] = lorem[i - 1].slice(0, -1) + ".";
-      }
-    }
+    //   if (previousWord.at(-1) === ",") {
+    //     lorem[i - 1] = lorem[i - 1].slice(0, -1) + ".";
+    //   }
+    // }
     
-    //Якщо позаду крапка заміни перший символ на великий
-    if (templorem.at(-1) === "." && lorem[i][0] === lorem[i][0].toLowerCase()) {
-      lorem[i] = lorem[i][0].toUpperCase() + lorem[i].slice(1);
-    }
+    // //Якщо позаду крапка заміни перший символ на великий
+    // if (previousWord.at(-1) === "." && lorem[i][0] === lorem[i][0].toLowerCase()) {
+    //   lorem[i] = lorem[i][0].toUpperCase() + lorem[i].slice(1);
+    // }
   }
   
   //напевно це переписати по іншому
